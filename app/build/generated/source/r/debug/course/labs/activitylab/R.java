720bb10b306eb532051fdf506492f989be0cfe89
Fool <mtahar1998@gmail.com>
2017-09-08 17:19:31 -0400

    overriden all lifecycles

--- app/build/generated/source/r/debug/course/labs/activitylab/R.java
+++ app/build/generated/source/r/debug/course/labs/activitylab/R.java
@@ -15,15 +15,22 @@ public final class R {
     }
     public static final class id {
         public static final int create=0x7f070000;
-        public static final int destroy=0x7f070006;
-        public static final int item1=0x7f070008;
-        public static final int item2=0x7f070009;
-        public static final int menu_settings=0x7f070007;
-        public static final int pause=0x7f070003;
-        public static final int restart=0x7f070005;
-        public static final int resume=0x7f070002;
-        public static final int start=0x7f070001;
-        public static final int stop=0x7f070004;
+        public static final int createL=0x7f070001;
+        public static final int destroy=0x7f07000c;
+        public static final int destroyL=0x7f07000d;
+        public static final int item1=0x7f07000f;
+        public static final int item2=0x7f070010;
+        public static final int menu_settings=0x7f07000e;
+        public static final int pause=0x7f070006;
+        public static final int pauseL=0x7f070007;
+        public static final int restart=0x7f07000a;
+        public static final int restartL=0x7f07000b;
+        public static final int resume=0x7f070004;
+        public static final int resumeL=0x7f070005;
+        public static final int start=0x7f070002;
+        public static final int startL=0x7f070003;
+        public static final int stop=0x7f070008;
+        public static final int stopL=0x7f070009;
     }
     public static final class layout {
         public static final int activity_one=0x7f030000;
@@ -54,8 +61,15 @@ public final class R {
     public static final class style {
         /**  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
          */
         public static final int AppBaseTheme=0x7f040000;
+        /**  All customizations that are NOT specific to a particular API-level can go here. 
+         */
         public static final int AppTheme=0x7f040001;
     }
 }
--- app/src/main/java/course/labs/activitylab/ActivityOne.java
+++ app/src/main/java/course/labs/activitylab/ActivityOne.java
@@ -8,6 +8,8 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class ActivityOne extends Activity {
 
 		// string for logcat documentation
@@ -16,14 +18,39 @@ public class ActivityOne extends Activity {
 		// lifecycle counts
 		//TODO: Create 7 counter variables, each corresponding to a different one of the lifecycle callback methods.
 		//TODO:  increment the variables' values when their corresponding lifecycle methods get called.
-		
+
+		private int createC = 0;
+		private int startC = 0;
+		private int resumeC = 0;
+		private int pauseC = 0;
+		private int stopC = 0;
+		private int destroyC =0;
+		private int restartC = 0;
+		TextView create, start, resume, pause, stop, destroy, restart;
+
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
+
+			this.createC++;
+
 			setContentView(R.layout.activity_one);
-			
+			this.create = (TextView) findViewById(R.id.createL);
+			create.setText(createC + "");
+			this.start = (TextView) findViewById(R.id.startL);
+			start.setText(startC + "");
+			this.resume = (TextView) findViewById(R.id.resumeL);
+			resume.setText(resumeC + "");
+			this.pause = (TextView) findViewById(R.id.pauseL);
+			pause.setText(pauseC + "");
+			this.stop = (TextView) findViewById(R.id.stopL);
+			stop.setText(stopC + "");
+			this.destroy = (TextView) findViewById(R.id.destroyL);
+			destroy.setText(destroyC + "");
+			this.restart = (TextView) findViewById(R.id.restartL);
+			restart.setText(restartC + "");
 			//Log cat print out
-			Log.i(TAG, "onCreate called");
+			Log.i(TAG, "onCreate called " + this.createC);
 			
 			//TODO: update the appropriate count variable & update the view
 		}
@@ -40,12 +67,56 @@ public class ActivityOne extends Activity {
 		@Override
 		public void onStart(){
 			super.onStart();
-			
+			this.startC++;
 			//Log cat print out
-			Log.i(TAG, "onStart called");
-			
+			Log.i(TAG, "onStart called " + this.startC);
+			this.start.setText(this.startC+"");
 			//TODO:  update the appropriate count variable & update the view
 		}
+
+		@Override
+		public void onResume(){
+			super.onResume();
+			this.resumeC++;
+			//Log cat print out
+			Log.i(TAG, "onResume called " + this.resumeC);
+			this.resume.setText(this.resumeC+"");
+		}
+	@Override
+	public void onPause(){
+		super.onPause();
+		this.resumeC++;
+		//Log cat print out
+		Log.i(TAG, "onPause called " + this.pauseC);
+		this.pause.setText(this.pauseC+"");
+	}
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        this.stopC++;
+        //Log cat print out
+        Log.i(TAG, "onStop called " + this.stopC);
+        this.stop.setText(this.stopC+"");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        this.destroyC++;
+        //Log cat print out
+        Log.i(TAG, "onDestroy called " + this.destroyC);
+        this.stop.setText(this.destroyC+"");
+    }
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        this.restartC++;
+        //Log cat print out
+        Log.i(TAG, "onRestart called " + this.restartC);
+        this.stop.setText(this.restartC+"");
+    }
+
+
 
 	    // TODO: implement 5 missing lifecycle callback methods
 
